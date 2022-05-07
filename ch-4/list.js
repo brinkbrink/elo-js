f11c7d216a176246fb19a0dc9c2155a092b2fead
E. Brink <brinkbrink@gmail.com>
2022-05-07 13:08:29 -0700

    completed

--- ch-4/list.js
+++ ch-4/list.js
@@ -1,5 +1,4 @@
-/*A nice thing about lists is that they can share parts of their structure. For example, if I create two new values {value: 0, rest: list} and {value: -1, rest: list} (with list referring to the binding defined earlier), they are both independent lists, but they share the structure that makes up their last three elements. The original list is also still a valid three-element list.
-Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+/*Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
 If you haven’t already, also write a recursive version of nth.*/
 
 arrayToList = (array) => {
@@ -10,8 +9,6 @@ arrayToList = (array) => {
     return newList;
 }
 
-console.log(arrayToList([10,20,30,4]));
-
 listToArray = (list) => {
     let array = [];
     for (let node = list; node; node = node.rest) {
@@ -20,11 +17,16 @@ listToArray = (list) => {
     return array;
 }
 
-console.log(listToArray(arrayToList([10,20,30,4])));
-
-let aList = arrayToList([10,20])
 prepend = (list, element) => {
     return {element, rest: list};
 }
-console.log(prepend(aList, 20));
-console.log(prepend(prepend(null, 20), 10));
+
+let nth = (list, n) => {
+    if(!list){
+        return undefined;
+    } else if(n==0){
+        return list.value;
+    } else {
+        return nth(list.rest, n-1);
+    }
+}
