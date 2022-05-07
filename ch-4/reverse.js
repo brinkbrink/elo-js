b8a235813d3665caa347a08ae73808e823bf0616
E. Brink <brinkbrink@gmail.com>
2022-05-07 12:06:48 -0700

    completed

--- ch-4/reverse.js
+++ ch-4/reverse.js
@@ -10,12 +10,9 @@ console.log(reverseArray([1,2,3,4]));
 
 reverseArrayInPlace = (array) => {
     for (let i = 0; i < Math.floor(array.length/2); i++) {
-        original = array[i];
+        let orig = array[i];
         array[i] = array[array.length - 1 - i];
-        array[array.length - 1 - i] = original;
+        array[array.length - 1 - i] = orig;
     }
     return array;
 }
-
-let numbers = [1, 2, 3, 4, 5];
-reverseArrayInPlace(numbers);
