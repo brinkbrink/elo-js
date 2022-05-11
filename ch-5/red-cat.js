42882b0c75b3870a416c636e23b195bc4370905b
E. Brink <brinkbrink@gmail.com>
2022-05-10 18:08:04 -0700

    completed

--- ch-5/red-cat.js
+++ ch-5/red-cat.js
@@ -1,3 +1,7 @@
 let cats = [["panda", "tigre", "jerry"],["fiona", "justine", "rastakitty"],["miss b","franny","abe"]].reduce((previousValue, currentValue)=>previousValue.concat(currentValue),[]);
 console.log(cats);
 
+// or
+
+let katz = [["panda", "tigre", "jerry"],["fiona", "justine", "rastakitty"],["miss b","franny","abe"]];
+console.log(katz.reduce((flat, current) => flat.concat(current), []));
